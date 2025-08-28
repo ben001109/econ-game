8c33bae88c3d0ddd28d7f9d1a051c099c143a03d
Huang YiKai <64959820+ben001109@users.noreply.github.com>
2025-08-29 03:22:14 +0800

    feat: 更新 tsconfig.json 配置，新增 lib 和 types 設定；新增 .eslintignore 檔案以排除不必要的目錄

---
+++ services/bot/.eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+};
+
